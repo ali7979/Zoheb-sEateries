e1b385c255d8892c671b38cbdb319bc86494f245
Zoheb Alam <zohebzob@gmail.com>
2024-06-02 01:54:41 +0530

    URL

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -28,7 +28,7 @@ const loginUser = async (req, res) => {
   catch (error) 
   {
     console.log(error);
-    res.json({ success: false, message: "Error" });
+    res.json({ success: false, message: error });
   }
 };
 
